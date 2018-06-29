2134c7acac5aaa1cdd76a900740b19b5d7eb3278
Malex <Malex@192.168.16.104>
2018-06-29 17:30:52 +0200

    updated test

--- thelibrary-core/src/test/java/com/benfante/javacourse/thelibrary/core/model/BookTest.java
+++ thelibrary-core/src/test/java/com/benfante/javacourse/thelibrary/core/model/BookTest.java
@@ -9,11 +9,12 @@ public class BookTest {
 
 	@Test
 	public void testBookConstruction() {
-		long id = 0;
+		long id = 1;
 		String title = "A title";
-		Author author = new Author(1,"Agatha","Christie");
+		Author author = new Author(1, "The", "single author");
+		Publisher publisher = new Publisher(1, "A publisher");
 		float price = 1.23f;
-		Book book = new Book(id, title, author, price);
+		Book book = new Book(id, title, author, publisher, price);
 		assertEquals(id, book.getId());
 		assertEquals(title, book.getTitle());
 		assertEquals(author, book.getAuthor()[0]);
@@ -22,22 +23,26 @@ public class BookTest {
 
 	@Test
 	public void testBookSetters() {
-		long id = 0;
+		long id = 1;
 		String title = "A title";
-		Author author = new Author(1,"Agatha","Christie");
+		Author author = new Author(1, "The", "single author");
+		Publisher publisher = new Publisher(1, "A publisher");
 		float price = 1.23f;
-		Book book = new Book(id, title, author, price);
-		long newId = 0;
-		String newTitle = "A special title";
-		Author newAuthor = new Author(4,"Geroge","Martin");
-		float newPrice = 1.23f;
+		Book book = new Book(id, title, author, publisher, price);
+		long newId = 2;
+		String newTitle = "Another title";
+		Author newAuthor = new Author(2, "Another", "author");
+		Publisher newPublisher = new Publisher(2, "Another publisher");
+		float newPrice = 2.34f;
 		book.setId(newId);
 		book.setTitle(newTitle);
 		book.setAuthor(newAuthor);
+		book.setPublisher(newPublisher);
 		book.setPrice(newPrice);
 		assertEquals(newId, book.getId());
 		assertEquals(newTitle, book.getTitle());
 		assertEquals(newAuthor, book.getAuthor()[1]);
+		assertEquals(newPublisher, book.getPublisher());
 		assertEquals(newPrice, book.getPrice(), 0.1);
 	}
 	
