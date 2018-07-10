d545795973f6b7b2cfc0c3ff8ce1a251e6e5d947
Malex <Malex@192.168.16.104>
2018-07-10 11:09:39 +0200

    added test

--- thelibrary-core/src/test/java/com/benfante/javacourse/thelibrary/core/model/BookIsbnComparatorTest.java
+++ thelibrary-core/src/test/java/com/benfante/javacourse/thelibrary/core/model/BookIsbnComparatorTest.java
@@ -2,6 +2,9 @@ package com.benfante.javacourse.thelibrary.core.model;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class BookIsbnComparatorTest {
@@ -22,4 +25,34 @@ public class BookIsbnComparatorTest {
 		assertTrue(com.compare(book1, book3)>0);
 	}
 
+	@Test
+	public void sortCompare() {
+		BookIsbnComparator com = BookIsbnComparator.getInstance();
+		
+		Book book1 = new Book(1,"A book",new Author(1,"a","author"));
+		Book book2 = new Book(1,"A book",new Author(1,"a","author"));
+		Book book3 = new Book(1,"A book",new Author(1,"a","author"));
+		Book book4 = new Book(1,"A book",new Author(1,"a","author"));
+		book1.setIsbn("12345");
+		book2.setIsbn("12345");
+		book3.setIsbn("02159");
+		book4.setIsbn("552369");
+		
+		List<Book> tmp = new LinkedList<>();
+		tmp.add(book4);
+		tmp.add(book3);
+		tmp.add(book2);
+		tmp.add(book1);
+		
+		List<Book> ordered = new LinkedList<>();
+		ordered.add(book3);
+		ordered.add(book2);
+		ordered.add(book1);
+		ordered.add(book4);
+		
+		
+		tmp.sort(com);
+		
+		assertEquals(tmp,ordered);
+	}
 }
