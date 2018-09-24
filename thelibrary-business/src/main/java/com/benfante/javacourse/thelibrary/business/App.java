3a209f7836a0d2ff111e0ec89392ca94beff37e3
Malex <Malex@malexprojects.com>
2018-09-24 14:47:47 +0200

    the library business project init

---
+++ thelibrary-business/src/main/java/com/benfante/javacourse/thelibrary/business/App.java
@@ -0,0 +1,12 @@
+package com.benfante.javacourse.thelibrary.business;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
